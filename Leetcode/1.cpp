18d0659d75ee232cb0ebd547986e26b2724c5a0d
Akshay <20bcs59@nith.ac.in>
2023-01-24 18:14:39 +0530

    leetcode

---
+++ Leetcode/1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> mp;
+        vector<int> ans(2);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int req = target - nums[i];
+            if (mp.find(req) != mp.end())
+            {
+                ans[0] = i;
+                ans[1] = mp[req];
+                return ans;
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
+int main()
+{
+
+    return 0;
+}
