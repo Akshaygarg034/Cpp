ee7dfd9b381871db5b5739bc12c68605655b3106
Akshay <20bcs59@nith.ac.in>
2023-02-01 22:34:56 +0530

    14

---
+++ Leetcode/14.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        string res = s[0];
+        int ans = s[0].size();
+        for(int i=1; i<s.size(); i++){
+            int j=0;
+            while(j<s[i].size() && s[i][j] == s[0][j])j++;
+            ans = min(ans, j);
+        }
+        return res.substr(0, ans);
+    }
+};
+
+int main(){
+    
+    return 0;
+}
