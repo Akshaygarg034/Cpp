5a0bb2e35e048ce8f3f6c02050d8a970126c7cf9
Akshay <20bcs59@nith.ac.in>
2023-02-01 21:41:47 +0530

    3 questions

---
+++ Leetcode/387.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> mp;
+        for(auto it : s){
+            mp[it]++;
+        }
+        for(int i=0; i<s.size(); i++){
+            if(mp[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
