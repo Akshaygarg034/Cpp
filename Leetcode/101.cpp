4ee05d59f54768f4377b9e38a42dcd0eedc3c07b
Akshay <20bcs59@nith.ac.in>
2023-02-01 23:24:07 +0530

    101

---
+++ Leetcode/101.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool sol(TreeNode *r1, TreeNode *r2)
+    {
+        if (!r1 && !r2)
+            return true;
+        if (!r1 || !r2 || r1->val != r2->val)
+            return false;
+        return sol(r1->left, r2->right) && sol(r1->right, r2->left);
+    }
+    bool isSymmetric(TreeNode *root)
+    {
+        return sol(root->left, root->right);
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
