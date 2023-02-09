6ae0d46f1e29f7cd7603248808a2ae6e07a69fee
Akshay <20bcs59@nith.ac.in>
2023-02-09 21:40:51 +0530

    104

---
+++ Leetcode/104.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    int solve(TreeNode *root, int depth)
+    {
+        if (root == NULL)
+            return depth;
+        int left = solve(root->left, depth + 1);
+        int right = solve(root->right, depth + 1);
+        return max(left, right);
+    }
+
+    int maxDepth(TreeNode *root)
+    {
+        return solve(root, 0);
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
