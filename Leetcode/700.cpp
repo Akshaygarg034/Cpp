fa50e6c9a59ecd28fbc9a898f8754f34888002a8
Akshay <20bcs59@nith.ac.in>
2023-03-27 22:59:20 +0530

    700

---
+++ Leetcode/700.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (!root || root->val == val)
+            return root;
+        if (val < root->val)
+            return searchBST(root->left, val);
+        if (val > root->val)
+            return searchBST(root->right, val);
+        return root;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
