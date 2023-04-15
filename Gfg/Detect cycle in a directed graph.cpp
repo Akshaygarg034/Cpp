cb5c8b347861b1f3d8519d286b5ea79e1ab9b4e0
Akshay <20bcs59@nith.ac.in>
2023-04-16 00:44:50 +0530

    detect cycle bfs

--- Gfg/Detect cycle in a directed graph.cpp
+++ Gfg/Detect cycle in a directed graph.cpp
@@ -15,34 +15,79 @@ struct Node
     }
 };
 
-class Solution {
-  public:
-    // Function to detect cycle in a directed graph.
-    void solve(vector<int> adj[], vector<int> &vis, vector<int> &order, int elem, int &f ){
-        vis[elem] = 1;
-        order[elem] = 1;
-        for(auto it: adj[elem]){
-            if(!vis[it]){
-                solve(adj, vis, order, it, f);
-                if(f) return;
-            } 
-            else if(order[it]){
-                 f=1;
-                 return;
+class Solution
+{
+public:
+    // DFS method to detect cycle in a directed graph.
+
+    // void solve(vector<int> adj[], vector<int> &vis, vector<int> &order, int elem, int &f ){
+    //     vis[elem] = 1;
+    //     order[elem] = 1;
+    //     for(auto it: adj[elem]){
+    //         if(!vis[it]){
+    //             solve(adj, vis, order, it, f);
+    //             if(f) return;
+    //         }
+    //         else if(order[it]){
+    //              f=1;
+    //              return;
+    //         }
+    //     }
+    //     order[elem] = 0;
+    // }
+
+    // bool isCyclic(int V, vector<int> adj[]) {
+    //     int f = 0;
+    //     vector<int> vis(V, 0);
+    //     vector<int> order(V, 0);
+    //     for(int i=0; i<V; i++){
+    //         if(!vis[i]){
+    //             solve(adj, vis, order, i, f);
+    //         }
+    //     }
+    //     return f;
+    // }
+
+    // BFS method to detect cycle in a directed graph.
+
+    bool isCyclic(int V, vector<int> adj[])
+    {
+        vector<int> in(V, 0);
+        queue<int> q;
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : adj[i])
+            {
+                in[it]++;
             }
         }
-        order[elem] = 0;
-    }
-    bool isCyclic(int V, vector<int> adj[]) {
-        int f = 0;
-        vector<int> vis(V, 0);
-        vector<int> order(V, 0);
-        for(int i=0; i<V; i++){
-            if(!vis[i]){
-                solve(adj, vis, order, i, f);
+        int c = 0;
+        for (int i = 0; i < V; i++)
+        {
+            if (in[i] == 0)
+            {
+                q.push(i);
+                c++;
+            };
+        }
+        while (!q.empty())
+        {
+            int t = q.front();
+            q.pop();
+            for (auto it : adj[t])
+            {
+                in[it]--;
+                if (in[it] == 0)
+                {
+                    q.push(it);
+                    c++;
+                };
             }
         }
-        return f;
+        if (c == V)
+            return false;
+        else
+            return true;
     }
 };
 
