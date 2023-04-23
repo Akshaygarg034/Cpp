47c72a7a437f6e0627a45a4e2ae36f1f9078f9bd
Akshay <20bcs59@nith.ac.in>
2023-04-23 22:20:14 +0530

    mst with both prims and kruskal

--- Gfg/Minimum Spanning Tree.cpp
+++ Gfg/Minimum Spanning Tree.cpp
@@ -5,7 +5,10 @@ using namespace std;
 class Solution
 {
 public:
+    // 	*******************Prim's Algorithm******************
+
     // 	Take priority queue and push {element, weight} in it.
+    //  If we push {i, j} in Priority queue, it will take minimum elements on top based upon i and not j
     //  Pop from queue (minimum weighted node), add in ans and push all its adj alements
     int spanningTree(int V, vector<vector<int>> adj[])
     {
@@ -34,6 +37,62 @@ public:
         }
         return ans;
     }
+};
+
+// 	*******************Kruskal's Algorithm*******************
+
+int checkPar(int elem, vector<int> par)
+{
+    if (par[elem] == elem)
+        return elem;
+    return par[elem] = checkPar(par[elem], par);
+}
+
+int spanningTree(int V, vector<vector<int>> adj[])
+{
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
+    vector<int> rank(V, 1);
+    vector<int> par(V);
+    // Initially Every element is parent of itself
+    for (int i = 0; i < V; i++)
+        par[i] = i;
+
+    for (int i = 0; i < V; i++)
+    {
+        for (auto it : adj[i])
+        {
+            q.push({it[1], {i, it[0]}});
+        }
+    }
+    int count = 0;
+    int ans = 0;
+    while (count < V - 1)
+    {
+        // Popping out edge with minimum weight
+        int wt = q.top().first;
+        int x = q.top().second.first;
+        int y = q.top().second.second;
+        q.pop();
+        // Converting elements to their parents
+        int p1 = checkPar(x, par);
+        int p2 = checkPar(y, par);
+        if (p1 != p2)
+        {
+            // union
+            if (rank[p1] < rank[p2])
+                par[p1] = p2;
+            else if (rank[p2] < rank[p1])
+                par[p2] = p1;
+            else
+            {
+                par[p1] = p2;
+                rank[p2]++;
+            }
+            ans += wt;
+            count++;
+        }
+    }
+    return ans;
 };
 
 int main()
