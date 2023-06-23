613a0e77dc5b50087a097fe413b0b01576aad215
Akshay garg <gargakshay034@gmail.com>
2023-06-23 20:52:05 +0530

    Update 56.cpp

--- Leetcode/56.cpp
+++ Leetcode/56.cpp
@@ -1,34 +1,28 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
+// Sort the array so that smaller intervals appear first.
+// After that push 1st interval in the res.
+// Traverse along the intervals.
+// interval[i][0] can be equal to res_interval[0] or greater than it.
+// If interval[i][0] is <= res_interval[1], then we will merge it.       e.g  res - [2, 3] , interval - [2, 4] => [2, 4]
+// otherwise we will not merge it.       e.g  res - [2, 3] , interval - [4, 5]
 
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>> &intervals)
-    {
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> res;
         sort(intervals.begin(), intervals.end());
-        vector<int> temp = intervals[0];
-        for (auto it : intervals)
-        {
-            if (it[0] <= temp[1])
-            {
-                temp[1] = max(it[1], temp[1]);
+
+        for(int i=0; i < intervals.size(); i++){
+            // Initial case || Not merging
+            if(res.empty() || intervals[i][0] > res.back()[1]){
+                res.push_back(intervals[i]);
             }
-            else
-            {
-                res.push_back(temp);
-                temp = it;
+
+            // Merging
+            else{
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
             }
         }
-        res.push_back(temp);
+
         return res;
     }
 };
-
-int main()
-{
-
-    return 0;
-}
