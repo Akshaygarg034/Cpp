11c2f8543fe738cc22b6cde8a16ba70ac0a39eaa
Akshay garg <gargakshay034@gmail.com>
2023-07-05 16:01:26 +0530

    Update 229.cpp

--- Leetcode/229.cpp
+++ Leetcode/229.cpp
@@ -1,54 +1,46 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    vector<int> majorityElement(vector<int> &nums)
-    {
-        int num1 = 0, num2 = 0, count1 = 0, count2 = 0;
-        vector<int> res;
-        for (auto it : nums)
-        {
-            if (it == num1)
-                count1++;
-            else if (it == num2)
-                count2++;
-            else if (count1 == 0)
-            {
-                num1 = it;
-                count1 = 1;
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        int cnt1 = 0 , cnt2 = 0;
+        int el1 = 0, el2 = 0;
+
+        // Same as majority element I question, but for two elements
+        for(int i=0; i<n; i++){
+            // nums[i] != el2 or el1 conditions are also included, so that el1 and el2 will be unique.
+            if(cnt1 == 0 && nums[i] != el2){
+                el1 = nums[i];
+                cnt1 = 1;
             }
-            else if (count2 == 0)
-            {
-                num2 = it;
-                count2 = 1;
+
+            else if(cnt2 == 0 && nums[i] != el1){
+                el2 = nums[i];
+                cnt2 = 1;
             }
-            else
-            {
-                count1--;
-                count2--;
+
+            else if(nums[i] == el1) cnt1++;
+
+            else if(nums[i] == el2) cnt2++;
+
+            else{
+                cnt1--; cnt2--;
             }
         }
-        count1 = count2 = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == num1)
-                count1++;
-            else if (nums[i] == num2)
-                count2++;
+
+        // To check whether el1 and el2 are required ans or not
+        cnt1= 0; cnt2 = 0;
+
+        for(auto it : nums){
+            if(it == el1) cnt1++;
+            else if(it == el2) cnt2++;
         }
-        if (count1 > nums.size() / 3)
-            res.push_back(num1);
-        if (count2 > nums.size() / 3)
-            res.push_back(num2);
-        return res;
+
+        if(cnt1 > n/3) ans.push_back(el1);
+        if(cnt2 > n/3) ans.push_back(el2);
+
+        // Because ans is required in sorted order
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
-
-int main()
-{
-
-    return 0;
-}
