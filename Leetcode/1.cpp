c824dd9c00a2be9092f5b17d040e9ef0a9795890
Akshay garg <gargakshay034@gmail.com>
2023-07-08 11:16:07 +0530

    Update 1.cpp

--- Leetcode/1.cpp
+++ Leetcode/1.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-class Solution
-{
+// Using Map (One pass)
+
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        map<int, int> mp;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
         vector<int> ans(2);
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int req = target - nums[i];
-            if (mp.find(req) != mp.end())
-            {
+        for(int i=0; i<nums.size(); i++){
+            int req_no = target - nums[i];
+            if(mp.find(req_no) != mp.end()){
                 ans[0] = i;
-                ans[1] = mp[req];
+                ans[1] = mp[req_no];
                 return ans;
             }
             mp[nums[i]] = i;
@@ -22,8 +17,23 @@ public:
         return ans;
     }
 };
- 
-int main()
-{
-   return 0;
-}
+
+// Using Two pointers (Not working on this code, just for understanding)
+
+// It is not working here because after sorting, indexes are changed, so we can't return correct indexes.
+
+// class Solution {
+// public:
+//     bool twoSum(vector<int>& nums, int target) {
+//         sort(nums.begin(), nums.end());
+
+//         int i=0, j = nums.size()-1;
+
+//         while(i < j){
+//             if(nums[i] + nums[j] < target) i++;
+//             else if(nums[i] + nums[j] > target) j--;
+//             else return true;
+//         }
+//         return false;
+//     }
+// };
