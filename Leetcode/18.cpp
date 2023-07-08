6ba37fe15b86c2d92b4f5632ced6ec35a48b26aa
Akshay garg <gargakshay034@gmail.com>
2023-07-08 13:30:29 +0530

    Update 18.cpp

--- Leetcode/18.cpp
+++ Leetcode/18.cpp
@@ -1,52 +1,45 @@
-#include<iostream>
-#include <bits/stdc++.h>
-using namespace std;
+// Two pointer approach
 
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
-        vector<int> elem(4,0);
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        
-        for(int i = 0; i<n; i++){
-            long long target_1 = target - nums[i];
-            
-            for(int j=i+1; j < n; j++){
-                long long target_2 = target_1 - nums[j];
-                int front = j+1, last = n-1;
-                
-                while(front < last){
-                    
-                    int sum = nums[front] + nums[last];
-                    if(sum < target_2)
-                        front++;
-                    else if(sum > target_2)
-                        last--;
+        vector<vector<int>> ans;
+
+        sort(nums.begin(),  nums.end());
+
+        for(int i=0; i<n-3; i++){
+            // To prevent duplicate ans
+            if(i > 0 && nums[i] ==  nums[i-1]) continue;
+
+            for(int j=i+1; j<n-2; j++){
+                // To prevent duplicate ans
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+
+                int left = j+1; int right = n-1;
+
+                while(left < right){
+                    // Sum is added like this, in order to prevent overflow
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[left];
+                    sum += nums[right];
+
+                    if(sum < target) left++;
+
+                    else if(sum > target) right--;
+
                     else{
-                        elem[0] = nums[i];
-                        elem[1] = nums[j];
-                        elem[2] = nums[front];
-                        elem[3] = nums[last];
-                        res.push_back(elem);
-                        
-                        while(front < last && nums[front] == elem[2])++front;
-                        
-                        while(last > front && nums[last] == elem[3])--last;
+                        ans.push_back({nums[i] , nums[j] , nums[left] , nums[right]});
+                        left++; right--;
+
+                        // To prevent duplicate ans
+                        while(left < right && nums[left] == nums[left-1]) left++;
+                        while(left < right && nums[right] == nums[right+1]) right--; 
                     }
                 }
-                
-                while(j+1 < n && nums[j] == nums[j+1])++j;
             }
-            
-            while(i+1 < n && nums[i] == nums[i+1])++i;
         }
-        return res;
+        return ans;
     }
 };
-
-int main(){
-   
-    return 0;
-}
