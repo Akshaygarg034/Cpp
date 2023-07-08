737d297fba36ca97fdb30668604dc8820237179b
Akshay garg <gargakshay034@gmail.com>
2023-07-08 12:07:56 +0530

    Update 15.cpp

--- Leetcode/15.cpp
+++ Leetcode/15.cpp
@@ -1,45 +1,35 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
+// Two pointer approach
 
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int> &nums)
-    {
-        vector<vector<int>> res;
-        int n = nums.size();
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < n - 2; i++)
-        {
-            if (i == 0 || nums[i] != nums[i - 1])
-            {
-                int left = i + 1, right = n - 1;
-                while (left < right)
-                {
-                    int sum = nums[i] + nums[left] + nums[right];
-                    if (sum < 0)
-                        left++;
-                    else if (sum > 0)
-                        right--;
-                    else
-                    {
-                        res.push_back({nums[i], nums[left], nums[right]});
-                        while (left < right && nums[left] == nums[left + 1])
-                            left++;
-                        while (left < right && nums[right] == nums[right - 1])
-                            right--;
-                        left++;
-                        right--;
-                    }
+        int n = nums.size();
+
+        for(int i=0; i < n-2; i++){
+            // In order to avoid duplicate results
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            
+            int j = i+1; int k = n-1;
+
+            while(j < k){
+                int sum  = nums[i]+ nums[j] + nums[k];
+                if(sum < 0) j++;
+
+                else if(sum > 0) k--;
+
+                else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++; k--;
+
+                    // In order to avoid duplicate results
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
-        return res;
+
+        return ans;
     }
 };
-
-int main()
-{
-    return 0;
-}
