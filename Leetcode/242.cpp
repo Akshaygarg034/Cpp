c2ee0e00f9e6eca24fe7a3750810d70fb946cc32
Akshay garg <gargakshay034@gmail.com>
2023-07-27 22:59:32 +0530

    Update 242.cpp

--- Leetcode/242.cpp
+++ Leetcode/242.cpp
@@ -1,25 +1,40 @@
-#include<iostream>
-#include <bits/stdc++.h>
-using namespace std;
+// Using Map
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         map<char, int> mp;
+//         for(auto it : s){
+//             mp[it]++;
+//         }
+//         for(auto it : t){
+//             if(mp.find(it) == mp.end()) return false;
+//             mp[it]--;
+//             if(mp[it] == 0) mp.erase(it);
+//         }
+//         if(mp.empty())return true;
+//         else return false;
+//     }
+// };
+
+// Using Array of size 26
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int> mp;
-        for(auto it : s){
-            mp[it]++;
+        if(s.size() != t.size()) return false;
+        
+        vector<int> arr(26, 0);
+
+        for(int i=0; i<s.size(); i++){
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
         }
-        for(auto it : t){
-            if(mp.find(it) == mp.end()) return false;
-            mp[it]--;
-            if(mp[it] == 0) mp.erase(it);
+
+        for(auto it: arr){
+            if(it != 0) return false;
         }
-        if(mp.empty())return true;
-        else return false;
+
+        return true;
     }
 };
-
-int main(){
-    
-    return 0;
-}
