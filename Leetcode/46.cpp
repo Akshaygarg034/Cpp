2352508c0fb2d96cca35c2018c4ed719eb39943a
Akshay garg <gargakshay034@gmail.com>
2023-08-02 23:52:56 +0530

    Update 46.cpp

--- Leetcode/46.cpp
+++ Leetcode/46.cpp
@@ -1,25 +1,26 @@
-#include<iostream>
-#include <bits/stdc++.h>
-using namespace std;
+//              ^
+//             [1, 2, 3]
+//            /    |     \
+//       ^           ^          ^
+//   [1, 2, 3]   [2, 1, 3]  [3, 2, 1]
+//        .        .            .
+//        .        .            .
+//        .        .            .
 
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<vector<int>> &res, int index){
-        if(index == nums.size()) res.push_back(nums);
-        for(int i=index; i<nums.size(); i++){
-            swap(nums[i], nums[index]);
-            solve(nums, res, index + 1);
-            swap(nums[i], nums[index]);
+    void solve(vector<int>& nums, vector<vector<int>> &res, int idx){
+        if(idx == nums.size()) res.push_back(nums);
+
+        for(int i = idx; i<nums.size(); i++){
+            swap(nums[idx], nums[i]);
+            solve(nums, res, idx + 1);
+            swap(nums[idx], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        solve(nums, res, 0);
+        solve(nums ,res, 0);
         return res;
     }
 };
-
-int main(){
-    
-    return 0;
-}
