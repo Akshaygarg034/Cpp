56435f3dfa571f6f4f6b74396cc91633c493893a
Akshay garg <gargakshay034@gmail.com>
2023-08-07 23:41:05 +0530

    Update 74.cpp

--- Leetcode/74.cpp
+++ Leetcode/74.cpp
@@ -9,7 +9,7 @@ public:
     {
         int m = matrix.size();
         int n = matrix[0].size();
-        int l = 0, h = m * n - 1;
+        int l = 0, h = m * n - 1; 
         while (l <= h)
         {
             int mid = (l + h) / 2;
